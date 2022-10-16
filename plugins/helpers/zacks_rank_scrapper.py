4a1ca2005bbb395a81d47c61c86fa73c8cf7a7d8
312 <henriquebritos@hotmail.com>
2022-10-16 18:54:05 -0300

    chores: Zacks downlaod refactor

--- plugins/helpers/zacks_rank_scrapper.py
+++ plugins/helpers/zacks_rank_scrapper.py
@@ -1,20 +1,31 @@
-#!/usr/bin/python3
-import aiohttp
+'''
+Downlaods zacks ranks data using webscrapping
+'''
 import asyncio
+import random
+import re
 from collections.abc import Iterable
+from dataclasses import dataclass
 from enum import Enum
 from fractions import Fraction
 from html.parser import HTMLParser
-import random
-import re
-from typing import Awaitable, NamedTuple
+from typing import Any, Awaitable, NamedTuple, Tuple
+
+import aiohttp
 
 
-class MyHtmlParser(HTMLParser):
+class ZacksTickerPage(HTMLParser):
+    """
+    Class used to acess the ticker page from zacks website and scrap and save rank information
+    """
+
     def __init__(self):
         super().__init__()
         self.scope_depth: int = 0
         self.data: list[str] = []
+
+    def error(self, message: str) -> Any:
+        '''Part of the HTLMParser ABC'''
 
     def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
         if tag == "p" and ("class", "rank_view") in attrs and self.scope_depth == 0:
@@ -33,6 +44,7 @@ class MyHtmlParser(HTMLParser):
                 self.data.append("\n")
 
     def result(self) -> list[str]:
+        """Format a document into a list of line"""
         lines: list[str] = []
         for line in "".join(self.data).split("\n"):
             stripped = line.strip()
@@ -41,7 +53,9 @@ class MyHtmlParser(HTMLParser):
         return lines
 
 
-class Rank(str, Enum):
+@dataclass
+class Rank(Enum):
+    """All zacs ranks options"""
     A = 'A'
     B = 'B'
     C = 'C'
@@ -50,7 +64,9 @@ class Rank(str, Enum):
     F = 'F'
 
 
+@dataclass
 class StockRank(NamedTuple):
+    """Ranks information about a stock"""
     zacks_rank: int
     value: Rank
     growth: Rank
@@ -61,7 +77,10 @@ class StockRank(NamedTuple):
 
 
 def extract_data(body: str) -> StockRank:
-    parser = MyHtmlParser()
+    """
+    Receives the body of a zacks rank page and returns a StockRank
+    dataclass with correpondent data"""
+    parser = ZacksTickerPage()
     parser.feed(body)
     lines = parser.result()
     if len(lines) < 4:
@@ -85,23 +104,30 @@ def extract_data(body: str) -> StockRank:
     return StockRank(zacks_rank, industry_rank=lines[2], industry=industry, **subrank)
 
 
-async def get_symbol_data(session: aiohttp.ClientSession, symbol: str) -> tuple[str, StockRank | Exception]:
+async def get_symbol_data(
+        session: aiohttp.ClientSession,
+        symbol: str) -> Tuple[str, StockRank | Exception]:
+    '''Receives a ticker as a input and returns StockRank data'''
     try:
         url = f"https://www.zacks.com/stock/quote/{symbol}"
         headers = {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0",
         }
-        # Seja legal com o servidor não sendo tão eficiente
+        # Slow, but nice to zack's servers
         await asyncio.sleep(random.random() * 50)
         async with session.get(url, headers=headers) as response:
             print(f"Status for {symbol}: {response.status}")
             extracted = extract_data(await response.text())
             return (symbol, extracted)
-    except Exception as e:
-        return (symbol, e)
+    # this error will be saved in the output
+    except Exception as error:  # pylint: disable=W0703
+        return (symbol, error)
 
 
 async def fetch_symbol_ranks(symbols: Iterable[str]) -> dict[str, StockRank | Exception]:
+    '''
+    Fetchs zacks data of a list of tickers
+    '''
     awaitables: list[Awaitable[tuple[str, StockRank | Exception]]] = []
     async with aiohttp.ClientSession() as session:
         for symbol in symbols:
@@ -110,27 +136,20 @@ async def fetch_symbol_ranks(symbols: Iterable[str]) -> dict[str, StockRank | Ex
     return dict(results)
 
 
-def generate_symbols(filename: str, ratio: Fraction = Fraction(1, 1)) -> Iterable[str]:
-    with open(filename, "r") as file:
-        while True:
-            line = file.readline()
-            if not line:
-                break
-            sym = line.strip()
-            if hash(sym) % ratio.denominator < ratio.numerator:
-                yield line.strip()
-
-
 async def main() -> None:
+    '''
+    Implements the scrapping process for loading ticker zacks data
+    '''
     # Fração para testar em subconjuntos de símbolos
     # export PYTHONHASHSEED=algum número fixa o subconjunto
     stock_ranks = await fetch_symbol_ranks(generate_symbols("lines.txt", Fraction(1, 100)))
-    with open("result.csv", "w") as f:
+    with open("result.csv", mode="w", encoding='utf-8') as f:
         f.write("sym\tzacks\tvalue\tgrowth\tmomtum\tvgm\tindustry\n")
         for (symbol, rank) in stock_ranks.items():
             if isinstance(rank, StockRank):
                 f.write(
-                    f"{symbol}\t{rank.zacks_rank}\t{rank.value}\t{rank.growth}\t{rank.momentum}\t{rank.vgm}\t{rank.industry}\n")
+                    (f"{symbol}\t{rank.zacks_rank}\t{rank.value}\t{rank.growth}\t"
+                        + f"{rank.momentum}\t{rank.vgm}\t{rank.industry}\n"))
             else:
                 f.write(f"{symbol}\t{rank!r}\n")
 
@@ -139,4 +158,4 @@ if __name__ == "__main__":
     # with open("test.html", "r") as f:
     #    print(extract_data(f.read()))
 
-# vim: ts=4:sw=4:et     
+# vim: ts=4:sw=4:et
